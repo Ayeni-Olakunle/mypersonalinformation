a13ccb8f8d101fc1b4678d44c8256a0ea325545e
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-11-25 13:47:44 +0100

    update

--- myInfomation/server.js
+++ myInfomation/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT_ENV || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db")
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/info", require("./routes/infoRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(cors());
 app.use(errorHandler)
 
 app.listen(port, () => {
